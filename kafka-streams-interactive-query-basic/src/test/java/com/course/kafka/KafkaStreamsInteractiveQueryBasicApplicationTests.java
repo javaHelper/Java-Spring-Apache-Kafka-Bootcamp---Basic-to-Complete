07b5012345d68bd66466d3d0549d4bc1a490d44e
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-11-07 14:02:22 +0530

    Add files via upload

---
+++ kafka-streams-interactive-query-basic/src/test/java/com/course/kafka/KafkaStreamsInteractiveQueryBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsInteractiveQueryBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
