7626dbf7ca80e61a40be729b4a3ed8b161258310
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-11-07 13:53:51 +0530

    removed

--- .sonarlint/default/.sonarlinttmp_8370499507185658218/eslint-bridge-bundle/package/lib/helpers/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './context';
-export * from './debug';
-export * from './files';
-export * from './timeout';
--- .sonarlint/default/.sonarlinttmp_8370499507185658218/eslint-bridge-bundle/package/lib/helpers/timeout.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import http from 'http';
-import express from 'express';
-declare function timeoutMiddleware(server: http.Server, shutdownTimeout: number): {
-    middleware(_request: express.Request, res: express.Response, next: express.NextFunction): void;
-    cancel(): void;
-};
-export { timeoutMiddleware };
