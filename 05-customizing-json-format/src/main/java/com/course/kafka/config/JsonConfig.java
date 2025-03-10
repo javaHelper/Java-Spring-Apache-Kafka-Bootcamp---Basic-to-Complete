548136f88ba1611ab8b7335d6430851c22a1dfaf
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-03-10 22:29:46 +0530

    Updated Spring Boot Dependency

--- 05-customizing-json-format/src/main/java/com/course/kafka/config/JsonConfig.java
+++ 05-customizing-json-format/src/main/java/com/course/kafka/config/JsonConfig.java
@@ -8,11 +8,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JsonConfig {
 
+//    @Bean
+//    public ObjectMapper objectMapper(){
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.findAndRegisterModules();
+//        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+//        return objectMapper;
+//    }
+
     @Bean
     public ObjectMapper objectMapper(){
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.findAndRegisterModules();
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
 }
