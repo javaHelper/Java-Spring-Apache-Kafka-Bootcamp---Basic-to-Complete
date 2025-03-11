4d8edb19074551d84e161fcfcaad4ab8145f50fe
Prateek <54174687+javaHelper@users.noreply.github.com>
2025-03-11 10:58:47 +0530

    commit

---
+++ 07-handling-consumer-offset/src/main/java/com/example/Application.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	private CounterProducer producer;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		//producer.sendMessage(100);
+	}
+}
